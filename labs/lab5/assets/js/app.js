f42669f30b77f59544418227737f3ed80ba56c5d
Charis <hulic436@newschool.edu>
2018-10-30 09:44:54 -0400

    yeth

--- labs/lab5/assets/js/app.js
+++ labs/lab5/assets/js/app.js
@@ -2,5 +2,24 @@ var name        = "Brian",
     age         = 33,
     can_drive   = true;
 
-var dresser = ['socks', 'shirts', 'pants', 'old stuff'];
+var dresser = [['socks', 25], 'shirts', 'pants', 'old stuff'];
 
+// function
+
+function hello_world(){
+    alert('hello world');
+}
+
+hello_world();
+
+function hello_world(){
+    alert('hello world');
+}
+
+hello_world();
+
+function hello_world(){
+    alert('hello world');
+}
+
+hello_world();
